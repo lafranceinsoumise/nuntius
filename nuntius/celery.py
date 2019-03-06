85efd919a664da94b456f8b608b752363a82ab98
Jill Royer <perso@guilro.com>
2019-03-06 16:22:14 +0100

    correct config for event reporting in celery

--- nuntius/celery.py
+++ nuntius/celery.py
@@ -3,9 +3,10 @@ from celery import Celery
 
 nuntius_celery_app = Celery("nuntius", set_as_current=False)
 
-nuntius_celery_app.conf.task_started = True
-
 nuntius_celery_app.config_from_object(
     "django.conf:settings", namespace="NUNTIUS_CELERY"
 )
+nuntius_celery_app.conf.worker_send_task_events = True
+nuntius_celery_app.conf.task_send_sent_event = True
+nuntius_celery_app.conf.task_track_started = True
 nuntius_celery_app.conf.task_routes = {"nuntius._tasks.*": {"queue": "nuntius"}}
