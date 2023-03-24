1b7c0d00e60a43ceaf60053ad57009c166b11d84
Salomé Cheysson <salome@cheysson.fr>
2023-03-24 17:21:18 +0100

    missing migration

--- nuntius/apps.py
+++ nuntius/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
 
-
 class NuntiusConfig(AppConfig):
     name = "nuntius"
+
+    default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
         from . import signals
